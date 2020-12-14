83297304d2482f2fea56ee364328e6e67af88138
entirelymagic <entirelymagic@gmail.com>
2020-12-14 16:28:10 +0200

    reformat files

--- ppf-ex06/solution/anim1.py
+++ ppf-ex06/solution/anim1.py
@@ -1,14 +1,16 @@
 import time
-target  = 50
-x       = 1
-dir     = 1
+
+
+target = 50
+x = 1
+direct = 1
 while True:
-    print("\r",end="")
+    print("\r", end="")
     j = 0
     while j < target:
-        print("#" if x==j else " ",end="")
-        j+=1
-    if x==0 or x==target:
-        dir*=-1
-    x+=dir
+        print("#" if x == j else " ", end="")
+        j += 1
+    if x == 0 or x == target:
+        direct *= -1
+    x += direct
     time.sleep(0.05)
