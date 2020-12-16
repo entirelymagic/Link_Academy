74673699d5e0b668af63542b9a1e688d770f2462
entirelymagic <entirelymagic@gmail.com>
2020-12-16 18:26:37 +0100

    Added function notes

--- Notes/Functions.py
+++ Notes/Functions.py
@@ -1,5 +1,28 @@
 "https://github.com/cosmin91ro/python-ppf"
 
+from typing import Any
 
 
+def greet(*string: str):
+    """Functie ce ia un paramentru pozitional -"""
+    return string
+
+
+a = greet("Salut", "Cristi")
+
+print(a)
+
+x, y, z = greet("Elvis", "Cosmin", "Florin")
+
+print(x, y, z)
+
+
+def f(*args):
+    """The function takes a list as an parameter and return the values within it if passed as arguments"""
+    print(args[0] + args[1])
+
+
+def g(**kwargs):
+    """"""
+    print(kwargs["firstname"])
 
