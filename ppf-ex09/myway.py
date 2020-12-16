28ed8b908360e05f9bb1f4ae9f3659c46ec583b1
entirelymagic <entirelymagic@gmail.com>
2020-12-16 17:32:55 +0100

    reformat

--- ppf-ex09/myway.py
+++ ppf-ex09/myway.py
@@ -1,30 +1,28 @@
-def render(w,h,path=[]): 
+def render(w, h, path=[]):
     for y in range(h):
-        for x in range(w): 
+        for x in range(w):
             pt = " "
-            if x==0 or x==w-1 or y==0 or y==h-1:
-                pt = "#" 
-            elif [x,y] in path:
+            if x == 0 or x == w - 1 or y == 0 or y == h - 1:
+                pt = "#"
+            elif [x, y] in path:
                 pt = "@"
-            print(pt,end="")
+            print(pt, end="")
         print()
 
-def path(start,end):
+
+def path(start, end):
     res = [start.copy()]
-    while True: 
-        lastpt = res[len(res)-1]
+    while True:
+        lastpt = res[len(res) - 1]
         newpt = lastpt.copy()
         if lastpt[0] < end[0]:
-            newpt[0]+=1
+            newpt[0] += 1
         if lastpt[1] < end[1]:
-            newpt[1]+=1
+            newpt[1] += 1
         if newpt == lastpt:
             break
         res.append(newpt)
     return res
-        
-
-render(20,15,path([2,3],[17,12])) 
 
 
-
+render(20, 15, path([2, 3], [17, 12]))
--- ppf-ex09/render.py
+++ ppf-ex09/render.py
@@ -1,11 +1,11 @@
-def render(w,h):
+def render(w, h):
     for y in range(h):
         for x in range(w):
-            if x==0 or x==w-1 or y==0 or y==h-1:
-                print("#",end="")
+            if x == 0 or x == w - 1 or y == 0 or y == h - 1:
+                print("#", end="")
             else:
-                print(" ",end="")
+                print(" ", end="")
         print()
 
-render(10,10)
 
+render(10, 10)
