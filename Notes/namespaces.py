2a6ee365be3a1d15b4a0c2c576123644987f58b8
entirelymagic <entirelymagic@gmail.com>
2020-12-24 13:22:32 +0200

    how to add a module to global

--- Notes/namespaces.py
+++ Notes/namespaces.py
@@ -1,2 +1,19 @@
-# namespaces are local variables.
+"""
+    Namespaces.
 
+    Module:
+    - Nu ar trebui sa contina cod care se executa explicit
+
+    Modulele pot fi adaugate la nivel global in 2 variante:
+
+    1. Adaugarea programabila:
+        import sys
+        sys.path.append("/mydir/someotherdir")
+
+    2. Adaugarea caii in variabila de sistem PYTHONPATH:
+
+        set PYTHONPATH = %PYTHONPATH%;c:/mydir/someotherdir
+
+
+
+"""
