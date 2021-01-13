1128fe0d7ae007b6e27151cff8d2372bb71fa4de
entirelymagic <entirelymagic@gmail.com>
2021-01-13 18:52:39 +0100

    Added exercise 1 for lesson 2

---
+++ python_oop/curs_2/exercitiu_1.py
@@ -0,0 +1,38 @@
+class Masina:
+    def __init__(self, marca, consum, cap_rezervor= 50):
+        self.marca = marca
+        self.consum = consum
+        self.cap_rezervor = cap_rezervor
+        self.cantitate_benzina = 0
+        self.km_parcursi = 0
+        self.bani_cheltuiti = 0
+        self.valoare_benzina = 0
+
+    def alimentare(self, cantitate, pret):
+        if cantitate <= self.cap_rezervor-self.cantitate_benzina:
+            self.cantitate_benzina += cantitate
+            self.valoare_benzina = cantitate*pret
+            self.pret_benzina = pret
+
+        else:
+            self.cantitate_benzina = self.cap_rezervor
+            return self.cantitate_benzina
+
+    def parcurgere_distanta(self, distanta):
+
+        benzina_consumata = (distanta*self.consum)/100
+
+        if self.cantitate_benzina >= benzina_consumata:
+            self.cantitate_benzina -= benzina_consumata
+            self.km_parcursi += distanta
+        else:
+            self.cantitate_benzina = 0
+            distanta_parcursa = self.cantitate_benzina/7*100
+            self.km_parcursi += distanta_parcursa
+
+
+mercedes = Masina("Eclass", 7)
+mercedes.alimentare(20)
+
+print(mercedes.cantitate_benzina)
+
