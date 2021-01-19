7da7b344985b8b903c4fb34fa6c66587104c6373
entirelymagic@gmail.com <@lexandr@123>
2021-01-19 14:04:30 +0200

    Helping my colleagues.
    Adding MRO (Method Resolution Order).

---
+++ python_oop/mro.py
@@ -0,0 +1,23 @@
+class A:
+    def process(self):
+        print('A process()')
+
+
+class B:
+    def process(self):
+        print('B process()')
+
+
+class C(A, B):
+    def process(self):
+        print('C process()')
+
+
+class D(C, B):
+    pass
+
+
+obj = D()
+obj.process()
+
+print(D.mro())
