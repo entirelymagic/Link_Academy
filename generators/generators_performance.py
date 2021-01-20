d61cbb3fd144560402d261ac8fe27fdd757ea58e
entirelymagic@gmail.com <@lexandr@123>
2021-01-20 11:24:42 +0200

    Added generators

--- generators/generators_performance.py
+++ generators/generators_performance.py
@@ -15,16 +15,21 @@ def performance(fn):
 @performance
 def long_time():
     print('1')
-    for i in range(100000):
+    for i in range(10000000):
         i * 5
 
 
 @performance
 def long_time2():
     print('2')
-    for i in list(range(100000)):
+    for i in list(range(10000000)):
         i * 5
 
 
 long_time()
 long_time2()
+
+
+def generator_function(num):
+    for i in range(num):
+        yield i * 2
