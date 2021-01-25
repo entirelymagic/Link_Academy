e2426bc775914a12dd33fc9c328a790c39a0eb74
entirelymagic@gmail.com <@lexandr@123>
2021-01-25 15:51:51 +0200

    refactoring

--- python_oop/automation.py
+++ python_oop/automation.py
@@ -1,7 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 """http://allselenium.info/python-selenium-commands-cheat-sheet-frequently-used/"""
 chrome_path = 'C:/Windows/chromedriver.exe'
 chrome_browser = webdriver.Chrome(chrome_path)
 
 chrome_browser.maximize_window()
+
+"""
+Browser Arguments:
+
+–headless
+
+To open browser in headless mode. Works in both Chrome and Firefox browser
+
+–start-maximized
+
+To start browser maximized to screen. Requires only for Chrome browser. Firefox by default starts maximized
+
+–incognito
+
+To open private chrome browser
+
+–disable-notifications
+
+To disable notifications, works Only in Chrome browser
+"""
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--start-maximized")
+options.add_argument("--disable-notifications")
+options.add_argument("--incognito")
+
+driver = webdriver.Chrome(chrome_options=options, executable_path="Path to driver")
--- python_oop/decorators.py
+++ python_oop/decorators.py
@@ -2,41 +2,25 @@ from time import time
 
 
 # Higher Order Functions:
-
-
 def greet(func):
-    """Higher Order Functions - because it is as a parameter another function"""
+    """Higher Order Functions - a function is parameter for another function"""
     func()
 
 
 def greet2():
-    """Higher Order Functions - because it is returning a function"""
-
+    """Higher Order Functions -return a function"""
     def func():
         return 5
-
     return func
 
 
 def star_super_booster(func):
     """A decorator that prints stars before and after a function execution"""
-
     def wrapper(*args, **kwargs):
         print('*' * 20)
         func(*args, **kwargs)
         print('*' * 20)
-
     return wrapper
-
-
-@star_super_booster
-def hello(name, emoji=':)'):
-    """A function that prints hello!"""
-    print(f'Hello {name}! {emoji}')
-
-
-hello('Elvis', ':D')
-hello('Alexandra')
 
 
 def performance(func):
@@ -46,23 +30,9 @@ def performance(func):
         t2 = time()
         print(f'Function "{func.__name__}" took {t2 - t1} seconds to execute.')
         return value
-
     return wrapper
 
 
-@performance
-def long_time():
-    total = 0
-    for i in range(10000000):
-        total += i * 5
-    return total
 
 
-z = 'Hello'
-z.replace('l', 'p', count=1)
-print(f'this is z= {z}')
-x = 1
-y = 2
 
-if x > y:
-    x + y
