c2d2bbc6489c935718faf2e7b07a058349302bb1
entirelymagic <entirelymagic@gmail.com>
2021-02-13 10:24:57 +0100

    Added tables for html and css

--- resources.py
+++ resources.py
@@ -1,7 +1,20 @@
 """
+Web Development :
+
 https://www.freecodecamp.org/
 
 https://developer.mozilla.org/
+
+https://css-tricks.com/
+https://css-tricks.com/snippets/css/complete-guide-grid/
+
+https://caniuse.com/   #functionalitate website versions
+
+https://codepen.io/
+
+https://medium.com/
+
+https://www.w3.org/TR/css-template-3/
 
 
 """
