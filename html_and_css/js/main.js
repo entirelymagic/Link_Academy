150b3e1581c236d97ae985eead0ddc51930e9a24
entirelymagic <entirelymagic@gmail.com>
2021-02-24 18:03:05 +0100

    Animation and JS.

---
+++ html_and_css/js/main.js
@@ -0,0 +1,12 @@
+const elv_name = "Elvis"
+
+
+setTimeout(initial_alert, 3000)
+
+
+
+function initial_alert() {
+    alert("Hello " + elv_name + " to JavaSCript");
+}
+
+document.write('This is added with JavaScript')
