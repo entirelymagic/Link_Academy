e2318dc3be1179df3c7a4b333c913797b6a70df7
entirelymagic <entirelymagic@gmail.com>
2021-02-24 21:10:37 +0100

    Added extended options using js.

--- html_and_css/js/DOM_planet/planet.js
+++
@@ -1,9 +0,0 @@
-// We get the paragraph with a specific id and asociate it with a variable.
-var redplanet = document.getElementById('redplanet');
-var blueplanet = document.getElementById('blueplanet');
-
-redplanet.innerHTML = "<h1>Welcome to Mars!</h1>"
-redplanet.classList.add("alert", "alert-danger")
-
-
-blueplanet.innerText = "Thank you for keeping me clean!"
