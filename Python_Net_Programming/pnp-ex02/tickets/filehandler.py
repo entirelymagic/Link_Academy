c6c232662fb934a88926116bbef34bd6cd08c3ef
entirelymagic@gmail.com <@lexandr@123>
2021-03-10 16:15:39 +0200

    Refactoring

--- Python_Net_Programming/pnp-ex02/tickets/filehandler.py
+++ Python_Net_Programming/pnp-ex02/tickets/filehandler.py
@@ -1,35 +1,40 @@
-import os 
+import os
 import datetime
 import shutil
 
 TicketFileName = "tickets.txt"
-LastTicket = -1 
+LastTicket = -1
 FileLocked = False
+
+
 def getlineparts(line):
     return line.split("|")
-def getlastticket(): 
+
+
+def getlastticket():
     global LastTicket
-    if(LastTicket > 0):
-        return LastTicket 
+    if (LastTicket > 0):
+        return LastTicket
     global FileLocked
     while FileLocked: pass
-    FileLocked = True 
+    FileLocked = True
     global TicketFileName
     try:
-        ticketfile = open(TicketFileName,"r") 
+        ticketfile = open(TicketFileName, "r")
         ln = ticketfile.readline()
-        while ln != "": 
-            id,_,_ = getlineparts(ln) 
-            ln = ticketfile.readline() 
+        while ln != "":
+            id, _, _ = getlineparts(ln)
+            ln = ticketfile.readline()
         ticketfile.close()
         LastTicket = int(id)
         FileLocked = False
         return LastTicket
     except:
-        print("Tickets file does not exist") 
+        print("Tickets file does not exist")
         LastTicket = 0
         FileLocked = False
         return LastTicket
+
 
 def insertticket():
     newTicketId = getlastticket() + 1
@@ -37,14 +42,15 @@ def insertticket():
     global LastTicket
     global TicketFileName
     while FileLocked: pass
-    FileLocked = True 
+    FileLocked = True
     ticketfile = open(TicketFileName, "a")
     print(f"Inserting ticket {newTicketId}")
-    ticketfile.write(f"{newTicketId}|{datetime.datetime.now()}|0\r\n")   
-    ticketfile.close() 
+    ticketfile.write(f"{newTicketId}|{datetime.datetime.now()}|0\r\n")
+    ticketfile.close()
     FileLocked = False
     LastTicket = newTicketId
     return newTicketId
+
 
 def checkout(code):
     ticket_valid = False
@@ -52,15 +58,15 @@ def checkout(code):
     global TicketFileName
     global FileLocked
     while FileLocked: pass
-    FileLocked = True 
-    try: 
-        ticketfile  = open(TicketFileName, "r")
+    FileLocked = True
+    try:
+        ticketfile = open(TicketFileName, "r")
     except:
         FileLocked = False
         return False
-    tmpfile     = open("ticketstmp.txt", "w")
+    tmpfile = open("ticketstmp.txt", "w")
     ln = ticketfile.readline()
-    while ln: 
+    while ln:
         parts = getlineparts(ln)
         if int(parts[0]) == code:
             if parts[2].strip() == "0":
@@ -70,9 +76,9 @@ def checkout(code):
                 tmpfile.writelines(ln)
         else:
             tmpfile.writelines(ln)
-        ln = ticketfile.readline() 
+        ln = ticketfile.readline()
     ticketfile.close()
     tmpfile.close()
-    shutil.move("ticketstmp.txt",TicketFileName)
+    shutil.move("ticketstmp.txt", TicketFileName)
     FileLocked = False
-    return ticket_valid 
+    return ticket_valid
