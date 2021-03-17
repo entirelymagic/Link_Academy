1eacd16addad5ca05af92b006265138386ddd45d
entirelymagic <entirelymagic@gmail.com>
2021-03-17 20:47:18 +0100

    Added course 4 NetProgramming

---
+++ Python_Net_Programming/exercises/tickets/server.py
@@ -0,0 +1,69 @@
+import socket
+import threading
+import persistentFileHandler
+
+
+def accept(sock):
+    conn, addr = sock.accept()
+    hh = HttpHandler(conn)
+    hh.daemon = True
+    hh.start()
+
+
+class HttpHandler(threading.Thread):
+    sock = None
+
+    def __init__(self, sock):
+        super().__init__()
+        self.sock = sock
+
+    def run(self):
+        request = self.sock.recv(1024).decode("utf-8")
+        parts = request.split("\r\n")
+
+        bodyData = parts[len(parts) - 1]
+
+        description = parts[0].split(" ")
+        resource = description[1].replace("/", "")
+        method = description[0]
+        code = "200 OK"
+
+        if not resource:
+            body = "This is not a valid page"
+            code = "404 Not Found"
+        else:
+            if resource == "insert":
+                body = persistentFileHandler.insertticket()
+            elif resource == "checkout":
+                if method.lower() != "post":
+                    body = "You must use post method on this page"
+                    code = "405 Method Not Allowed"
+                elif not bodyData:
+                    body = "Not a valid body"
+                    code = "400 Bad Request"
+                else:
+                    print(f"Validating ticket {bodyData}")
+                    if not bodyData.isnumeric():
+                        body = "Not a valid ticket code"
+                        code = "400 Bad Request"
+                    else:
+                        body = persistentFileHandler.checkout(bodyData)
+            else:
+                body = "Not a valid command"
+                code = "400 Bad Request"
+
+            print(F"Returning {body}")
+
+        protocol = "HTTP/1.1"
+        self.sock.send(
+            f"{protocol} {code}\r\nContent-Type: text/html\r\nConnection: Close\r\n\r\n{body}".encode("utf-8"))
+        self.sock.close()
+
+
+sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sSocket.bind(("0.0.0.0", 8005))  # listen all ips
+sSocket.listen()
+
+while True:
+    accept(sSocket)
